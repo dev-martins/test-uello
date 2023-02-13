407d5939b8bea2aebd94a8f40a91de6c75b0ca98
Claudio Martins <claudio.martins@taxpricingit.com.br>
2023-02-13 20:00:31 -0300

    conformidade com as psr1 e psr12

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,9 +8,15 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * Class User
+ * @package App\Models
+ */
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
