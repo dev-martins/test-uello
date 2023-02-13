d28991c98fc1487269f44097067b4e0d402a2fd6
Claudio Martins <claudio.martins@taxpricingit.com.br>
2023-02-13 20:06:30 -0300

    roteamento da aplicacao

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('freight')->group(function () {
-    Route::get('reader', [ImportFreightController::class, 'headerCSV']);
     Route::post('import', [ImportFreightController::class, 'store']);
 });
