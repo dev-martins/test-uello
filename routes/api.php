ab3e696f235019191bfda4b005c743affb0b6dd0
Claudio Martins <claudio.martins@taxpricingit.com.br>
2023-02-13 20:06:10 -0300

    roteamento da aplicacao

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ImportFreightController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('freight')->group(function () {
+    Route::get('reader', [ImportFreightController::class, 'headerCSV']);
+    Route::post('import', [ImportFreightController::class, 'store']);
 });
